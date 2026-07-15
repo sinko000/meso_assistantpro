38f3a055df9f3041ef5e9ce9550880db995cad68
sinko000 <samiroglu70@gmail.com>
2026-07-15 21:15:00 +0400

    Refactor server listen method for better logging

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const express = require('express');
 
 const app = express();
 app.get('/', (req, res) => res.send('Bot is online and running!'));
-app.listen(process.env.PORT || 3000);
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
 const client = new Client({
     intents: [
